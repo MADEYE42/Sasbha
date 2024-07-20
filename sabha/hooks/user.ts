47f4b91be781bf7b3af8cc919e91045caf740525
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-07-20 22:59:56 +0530

    Update user.ts

--- sabha/hooks/user.ts
+++ sabha/hooks/user.ts
@@ -1,8 +1,10 @@
 import { create } from "zustand";
 interface User{
     name:string;
+    setName:(name:string)=>void;
 }
 export const useUser = create<User>((set)=>({
     name:'',
     setName:(name:string)=>set({name}),
-}))
+}));
+export default useUser
